03a4eb4a6e8bfb5e6242cf3163fa4df15920ef83
ibn-misbah <kawnuma@gmail.com>
2026-03-26 11:52:04 +0300

    feat: auth controller

---
+++ server/src/module/auth/auth.controller.ts
@@ -0,0 +1,76 @@
+import { Request, Response, NextFunction } from "express";
+import { loginInputSchema } from "./auth.schema";
+import { prisma } from "../../shared/prisma";
+import bcrypt from "bcryptjs";
+import { AuthService } from "./auth.service";
+
+// export const authController = {
+//   login: async (req: Request, res: Response) => {
+//     try {
+//       const { email, password } = loginInputSchema.parse(req.body);
+
+//       const user = await prisma.user.findUnique({
+//         where: { email: email },
+//       });
+
+//       if (!user) {
+//         res.status(404).json({
+//           success: false,
+//           code: "USER_NOT_FOUND",
+//           message: "Invalid Credentials",
+//         });
+//       }
+
+//       const isPasswordValid = await bcrypt.compare(
+//         password,
+//         user?.password as string,
+//       );
+
+//       if (!isPasswordValid) {
+//         res.status(400).json({
+//           success: false,
+//           code: "INVALID_PASSWORD",
+//           message: "Invalid Credentials",
+//         });
+//       }
+
+//       return res.status(200).json({
+//         success: true,
+//         code: "LOGIN_SUCCESS",
+//         data: {
+//           id: user?.id,
+//           fname: user?.firstName,
+//           lname: user?.lastName,
+//           role: user?.role,
+//           schoolId: user?.schoolId,
+//         },
+//       });
+//     } catch (error) {
+//       console.log("Login error:", error);
+//       return res.status(500).json({
+//         success: false,
+//         code: "ERROR_LOGIN",
+//         message: error,
+//       });
+//     }
+//   },
+// };
+
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const validatedData = loginInputSchema.parse(req.body);
+      const result = await this.authService.login(validatedData);
+
+      return res.json({
+        success: true,
+        data: result,
+        message: "Login Successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
